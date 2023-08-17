8023987d8f0d6423b3b4260f83f1479365c82dcb
張昭雯 <chaowen@zhangzhaowendeMacBook-Air.local>
2023-08-17 17:52:22 +0800

    init: vite+vue3+tailwindcss+fulpage.js

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- <full-page ref="fullpage" :options="options">
+    <div class="section">
+      <button class="next" @click="$refs.fullpage.api.moveSectionDown()">
+        Next
+      </button>
+      Section 1
+    </div>
+    <div class="section">
+      <button class="prev" @click="$refs.fullpage.api.moveSectionUp()">
+        Prev
+      </button>
+      Section 2
+    </div>
+  </full-page> -->
+
+  <full-page ref="fullpage" :options="options" id="fullpage">
+    <div class="section">First section ...</div>
+    <div class="section">Second section ...</div>
+  </full-page>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+export default {
+  setup() {
+    const options = {
+      licenseKey: 'gplv3-license',
+      menu: '#menu',
+      anchors: ['page1', 'page2', 'page3'],
+      sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+    };
+    onMounted(() => {
+      // hide fullpage watermark
+      const watermark = document.getElementsByClassName('fp-watermark');
+      watermark[0].style.display = 'none';
+    })
+    return {
+      options
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* not working */
+/* .fp-watermark {
+  display: none !important;
+} */
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// fullpage
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
+
+createApp(App).use(VueFullPage).mount('#app')
